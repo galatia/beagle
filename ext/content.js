99ffc2fc597f5e84c41a88e19bc02866ff13c270
galatia <g@latia.io>
2015-06-09 01:07:01 -0400

    pass paper url to meteor; fullscreen meteor

--- ext/content.js
+++ ext/content.js
@@ -1,10 +1,13 @@
-alert('Hello from the Beagle Chrome extension!')
+//alert('Hello from the Beagle Chrome extension!')
 
-document.getElementsByTagName('embed')[0].setAttribute('width', '75%')
+document.getElementsByTagName('embed')[0].remove()
+
+var paperUrl = window.location.href
 
 var sidebar = document.createElement('iframe')
-sidebar.setAttribute('src',   'http://QfjWnhEakV93XtaEjWaxqFZws.meteor.com')
-sidebar.setAttribute('width', '25%')
+sidebar.setAttribute('src',   'http://localhost:3000/url/'+encodeURIComponent(paperUrl))//'http://QfjWnhEakV93XtaEjWaxqFZws.meteor.com')
+sidebar.setAttribute('width', '100%')
 sidebar.setAttribute('height', '100%')
 sidebar.setAttribute('style', 'position:absolute; right:0; background-color:white')
 document.getElementsByTagName('body')[0].appendChild(sidebar)
+
--- meteor/meteor.js
+++ meteor/meteor.js
@@ -1,19 +1,11 @@
+Router.route('/url/:paperUrl', function () {
+    this.render('paper', {data: this.params});
+    var paperUrl = this.params.paperUrl
+});
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
