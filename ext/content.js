a2239ad17160fb5a203afc2376d75982353d9406
galatia <g@latia.io>
2015-06-12 04:28:13 -0400

    appHost var

--- ext/content.js
+++ ext/content.js
@@ -1,13 +1,13 @@
 //alert('Hello from the Beagle Chrome extension!')
 
-document.getElementsByTagName('embed')[0].remove()
-
+var appHost  = 'http://localhost:3000/' //'http://QfjWnhEakV93XtaEjWaxqFZws.meteor.com'
 var paperUrl = window.location.href
 
+document.getElementsByTagName('embed')[0].remove()
+
 var sidebar = document.createElement('iframe')
-sidebar.setAttribute('src',   'http://localhost:3000/url/'+encodeURIComponent(paperUrl))//'http://QfjWnhEakV93XtaEjWaxqFZws.meteor.com')
+sidebar.setAttribute('src', appHost+'url/'+encodeURIComponent(paperUrl))
 sidebar.setAttribute('width', '100%')
 sidebar.setAttribute('height', '100%')
 sidebar.setAttribute('style', 'position:absolute; right:0; background-color:white')
 document.getElementsByTagName('body')[0].appendChild(sidebar)
-
