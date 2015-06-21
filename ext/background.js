668905757c9a1e2b83cd73511197484294d79a91
galatia <g@latia.io>
2015-06-21 14:56:49 -0400

    more reliable port connection

--- ext/background.js
+++ ext/background.js
@@ -1,19 +1,19 @@
 var appHost  = 'http://localhost:3000/' //'http://QfjWnhEakV93XtaEjWaxqFZws.meteor.com'
 
-ports = []
+ports = {}
 var listener = function (port) {
-  ports.push(port);
-  console.log(port);
-  if (ports.length == 2){
-    var port0 = ports[0], port1 = ports[1]
-    ports = [] // clear for use in new tab
+  var id = port.sender.tab.id
+  if(!ports[id]) {ports[id]={}}
+  ports[id][port.sender.id? 'ext':'meteor']=port;
+  var port0 = ports[id].ext, port1 = ports[id].meteor
+  if (port0 && port1){
     // Listen for message from one and post to other; then the second one
-    port0.onMessage.addListener(function(message){
-      port1.postMessage(message)
-    })
-    port1.onMessage.addListener(function(message){
-      port0.postMessage(message)
-    })
+    if(port0.listener) { port0.onMessage.removeListener(port0.listener) }
+    if(port1.listener) { port1.onMessage.removeListener(port1.listener) }
+    port0.listener = function(message){ port1.postMessage(message) }
+    port1.listener = function(message){ port0.postMessage(message) }
+    port0.onMessage.addListener(port0.listener)
+    port1.onMessage.addListener(port1.listener)
   }
 }
 
